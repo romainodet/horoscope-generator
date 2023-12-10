7e9b53383598b81fd16b38ef9645e0a2a593bd1a
Romain ODET <romain@romainodet.fr>
2023-12-11 00:25:22 +0100

    correct code

--- static/script.js
+++ static/script.js
@@ -26,6 +26,17 @@ document.addEventListener("DOMContentLoaded", function () {
             });
     }
 
+    // Fonction pour actualiser les horoscopes
+    function refreshHoroscopes() {
+        // Effacez tous les horoscopes existants
+        horoscopesContainer.innerHTML = "";
+
+        // Récupérez les horoscopes pour chaque signe et les affichez à nouveau
+        signesDuZodiaque.forEach(signe => {
+            getHoroscope(signe);
+        });
+    }
+
     // Liste des signes du zodiaque à afficher
     const signesDuZodiaque = ['bélier', 'taureau', 'gemeaux', 'cancer', 'lion', 'vierge', 'balance', 'scorpion', 'sagittaire', 'capricorne', 'verseau', 'poissons'];
 
@@ -33,55 +44,71 @@ document.addEventListener("DOMContentLoaded", function () {
     signesDuZodiaque.forEach(signe => {
         getHoroscope(signe);
     });
-// Fonction pour activer le thème sombre
-function activateDarkMode() {
-    document.body.classList.add('dark-mode');
-    localStorage.setItem('theme', 'dark'); // Enregistrez le choix de l'utilisateur dans le stockage local
- // Changez la couleur de remplissage de l'icône en mode sombre
-    const themeIcon = document.getElementById('theme-icon');
-    themeIcon.style.fill = '#333'; // Couleur sombre
-}
 
-// Fonction pour désactiver le thème sombre
-function deactivateDarkMode() {
-    document.body.classList.remove('dark-mode');
-    localStorage.setItem('theme', 'light'); // Enregistrez le choix de l'utilisateur dans le stockage local
+    // Fonction pour activer le thème sombre
+    function activateDarkMode() {
+        document.body.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark'); // Enregistrez le choix de l'utilisateur dans le stockage local
+        // Changez la couleur de remplissage de l'icône en mode sombre
+        const themeIcon = document.getElementById('theme-icon');
+        themeIcon.style.fill = '#333'; // Couleur sombre
+
+        // Changez également la couleur de remplissage de l'icône du bouton de rafraîchissement en mode sombre
+        const refreshIcon = document.getElementById('refresh-button').querySelector('svg');
+        refreshIcon.style.fill = '#333'; // Couleur sombre
+    }
+
+    // Fonction pour désactiver le thème sombre
+    function deactivateDarkMode() {
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('theme', 'light'); // Enregistrez le choix de l'utilisateur dans le stockage local
         // Changez la couleur de remplissage de l'icône en mode clair
-    const themeIcon = document.getElementById('theme-icon');
-    themeIcon.style.fill = '#fff'; // Couleur claire
-}
+        const themeIcon = document.getElementById('theme-icon');
+        themeIcon.style.fill = '#fff'; // Couleur claire
 
-// Détecter les préférences de thème du système
-const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
-
-// Vérifiez si l'utilisateur a déjà fait un choix de thème
-const userTheme = localStorage.getItem('theme');
-
-if (userTheme === 'dark') {
-    activateDarkMode();
-} else if (userTheme === 'light') {
-    deactivateDarkMode();
-} else if (prefersDarkMode.matches) {
-    activateDarkMode(); // Activer le thème sombre si le système préfère le mode sombre
-}
-
-// Écouter les changements de préférences de thème du système
-prefersDarkMode.addEventListener('change', (event) => {
-    if (event.matches) {
-        activateDarkMode(); // Activer le thème sombre lorsque les préférences du système passent en mode sombre
-    } else {
-        deactivateDarkMode(); // Désactiver le thème sombre lorsque les préférences du système passent en mode clair
+        // Changez également la couleur de remplissage de l'icône du bouton de rafraîchissement en mode clair
+        const refreshIcon = document.getElementById('refresh-button').querySelector('svg');
+        refreshIcon.style.fill = '#fff'; // Couleur claire
     }
-});
 
-// Gestion du bouton de basculement
-const toggleButton = document.getElementById('toggle-theme');
+    // Détecter les préférences de thème du système
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
 
-toggleButton.addEventListener('click', () => {
-    if (document.body.classList.contains('dark-mode')) {
-        deactivateDarkMode(); // Si le mode sombre est actif, désactivez-le
-    } else {
-        activateDarkMode(); // Sinon, activez le mode sombre
+    // Vérifiez si l'utilisateur a déjà fait un choix de thème
+    const userTheme = localStorage.getItem('theme');
+
+    if (userTheme === 'dark') {
+        activateDarkMode();
+    } else if (userTheme === 'light') {
+        deactivateDarkMode();
+    } else if (prefersDarkMode.matches) {
+        activateDarkMode(); // Activer le thème sombre si le système préfère le mode sombre
     }
-});
+
+    // Écouter les changements de préférences de thème du système
+    prefersDarkMode.addEventListener('change', (event) => {
+        if (event.matches) {
+            activateDarkMode(); // Activer le thème sombre lorsque les préférences du système passent en mode sombre
+        } else {
+            deactivateDarkMode(); // Désactiver le thème sombre lorsque les préférences du système passent en mode clair
+        }
+    });
+
+    // Gestion du bouton de basculement
+    const toggleButton = document.getElementById('toggle-theme');
+
+    toggleButton.addEventListener('click', () => {
+        if (document.body.classList.contains('dark-mode')) {
+            deactivateDarkMode(); // Si le mode sombre est actif, désactivez-le
+        } else {
+            activateDarkMode(); // Sinon, activez le mode sombre
+        }
+    });
+
+    // Gestion du bouton d'actualisation
+    const refreshButton = document.getElementById('refresh-button');
+
+    refreshButton.addEventListener('click', () => {
+        refreshHoroscopes(); // Actualisez les horoscopes lorsque le bouton est cliqué
+    });
 });
